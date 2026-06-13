fe0eb37818fa19a220ee72d98bd47421bd6bd744
Abhijeet Arun Khatavkar <50583556+abhijeet101995@users.noreply.github.com>
2026-06-13 16:46:02 +0530

    new

--- Terraform/syntax.tf
+++ Terraform/syntax.tf
@@ -1,11 +1,16 @@
 provider "aws" {
-  region     = "us-west-2"
-  access_key = "my-access-key"
-  secret_key = "my-secret-key"
+  region  = "us-east-2"
+  profile = "configs"
 }
 
 resource "aws_ec2" "webserver"
 {
+   key_name = 
+   vpc_security_group_ids =
+   disable_api_termination
 
-
+   tags
+   {
+    name ="webserver"
+   }
 }
