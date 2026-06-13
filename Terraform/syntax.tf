dcb9c3728bfd245712fa5848041e713b5f731a7d
Abhijeet Arun Khatavkar <50583556+abhijeet101995@users.noreply.github.com>
2026-06-13 17:34:28 +0530

    new

--- Terraform/syntax.tf
+++ Terraform/syntax.tf
@@ -5,7 +5,7 @@ provider "aws" {
 
 resource "aws_instance" "webserver" {
 ami           = "ami-0741dc526e1106ae5"
-   instance_type = "t3micro"
+   instance_type = "t3.micro"
 key_name      = "abhijeet"
  vpc_security_group_ids = ["sg-0e924451d7773195c"]  # <--- Uses list syntax
   tags = { 
