3777b6dc2a01b60e864991165ba24a4ef0e8062d
Abhijeet Arun Khatavkar <50583556+abhijeet101995@users.noreply.github.com>
2026-06-13 17:06:57 +0530

    updates

--- Terraform/syntax.tf
+++ Terraform/syntax.tf
@@ -7,7 +7,7 @@ resource "aws_ec2" "webserver"{
    key_name = "abhijeet"
    ami= "ami-0741dc526e1106ae5"
    instance_type="t3.micro"
-   vpc_security_group_ids ={"sg-0e924451d7773195c"}
+   vpc_security_group_ids = {"sg-0e924451d7773195c"}
 tags
    {
     name ="webserver"
