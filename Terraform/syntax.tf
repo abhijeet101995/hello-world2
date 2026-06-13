84f2576349ec9134e2c1706485c1ef73bf3c04c1
Abhijeet Arun Khatavkar <50583556+abhijeet101995@users.noreply.github.com>
2026-06-13 17:12:23 +0530

    new changes

--- Terraform/syntax.tf
+++ Terraform/syntax.tf
@@ -3,15 +3,12 @@ provider "aws" {
   profile = "configs"
 }
 
-resource "aws_ec2" "webserver"{
-   key_name = "abhijeet"
-   ami= "ami-0741dc526e1106ae5"
-   instance_type="t3.micro"
-   vpc_security_group_ids = {"sg-0e924451d7773195c"}
-tags
-   {
-    name ="webserver"
-   }
+resource "aws_instance" "webserver" {
+  ami           = "ami-0741dc526e1106ae5"
+  instance_type = "t3micro"
+  key_name      = "abhijeet"
+vpc_security_group_ids = {"sg-0e924451d7773195c"}
+  tags {   
+    Name = "Web-Server"
+  }
 }
-
-# disable_api_termination =true
